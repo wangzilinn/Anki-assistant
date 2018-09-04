abdf75072de7949651705d959f1f7bf7a7c2f411
Wangzilin <wangzilinn@gmail.com>
2018-09-04 23:43:27 +0800

    增加静态类，方法类 将字典种类改为存入元组中

--- OutputWordLstToAniUseClass.py
+++ OutputWordLstToAniUseClass.py
@@ -8,9 +8,19 @@ from bs4 import BeautifulSoup
 import pyexcel_xlsx
 
 
+class Methods:
+    """各种小的方法函数"""
+    @staticmethod
+    def read_xlsx():
+        file = tk.filedialog.askopenfilename(filetypes=[("excel文件", ".xlsx")])
+        xls_data = pyexcel_xlsx.get_data(file)
+        for sheet in xls_data.keys():
+            return xls_data[sheet]  # 仅返回第一个表
+
+
 class Translator:
     """将英文单词翻译成带有翻译、例句等的字典"""
-    __dictionary_type_dict = {"youdao": 1, "iciba": 2, "youdict": 3}
+    dictionary_type_tuple = ("youdao", "iciba", "youdict")
     __selected_dictionary_type = 0
     __word = ""
     __result_dictionary = {}
@@ -28,14 +38,13 @@ class Translator:
         self.__load_result_dictionary(dictonary_type)
 
     def __load_result_dictionary(self, dictonary_type):
-        try:
-            if self.__dictionary_type_dict[dictonary_type] == 1:
-                self.__bing_dictionary()
-            elif self.__dictionary_type_dict[dictonary_type] == 2:
-                self.__iciba_dictionary()
-            elif self.__dictionary_type_dict[dictonary_type] == 3:
-                self.__youdict_dictionary()
-        except KeyError:
+        if self.dictionary_type_tuple[0] == dictonary_type:
+            self.__bing_dictionary()
+        elif self.dictionary_type_tuple[1] == dictonary_type:
+            self.__iciba_dictionary()
+        elif self.dictionary_type_tuple[2] == dictonary_type:
+            self.__youdict_dictionary()
+        else:
             self.__result_dictionary["error"] = "No dictionary found"
 
     def get_result_dictionary(self):
@@ -109,6 +118,7 @@ class Translator:
         # 释义:
         try:
             expresses = soup.find(id='yd-word-meaning')
+            express = ""
             for item in expresses:
                 express = item.text + express
             self.__result_dictionary["translation"] = re.sub('\s', '', express)
@@ -153,18 +163,18 @@ class Framework(tk.Tk):
     def __place_widgets(self):
         """放置各种widgets"""
         self.label_source_choice = tk.Label(text="choice source :")
-        self.label_source_choice.grid(row=0, column=1)
+        self.label_source_choice.grid(row=0, column=0)
 
         # 单词输入框+确认按钮
         self.label_word = tk.Label(text="word:")
-        self.label_word.grid(row=0, column=3)
+        self.label_word.grid(row=0, column=2)
 
         self.entry_word = tk.Entry()
-        self.entry_word.grid(row=1, column=3)
+        self.entry_word.grid(row=1, column=2)
 
-        # self.button_word_confirm = tk.Button(window, text="confirm", command=self.__command_button_confirm())
-        self.button_word_confirm = tk.Button(text="confirm")
-        self.button_word_confirm.grid(row=1, column=4)
+        self.button_word_confirm = tk.Button(text="confirm", command=self.__command_button_word_confirm)
+        # self.button_word_confirm = tk.Button(text="confirm")
+        self.button_word_confirm.grid(row=1, column=3)
 
         # 词汇表（CET4，考研等）
         self.label_vocabulary = tk.Label()
@@ -174,21 +184,44 @@ class Framework(tk.Tk):
         self.text_show_all = tk.Text(width=60, height=24)
         self.text_show_all.grid(row=5, column=0, columnspan=6, rowspan=6, padx=10)
 
-    def __command_button_confirm(self):
+        # 将text控件中的文档按单词本格式输出
+        self.button_output_confirm = tk.Button(text="output", command=self.__command_button_output_confirm)
+        self.button_output_confirm.grid(row=12, column=2, pady=10)
+
+        # 选择词典控件
+        self.__selected_dictionary_type = tk.StringVar(self)
+        self.__selected_dictionary_type.set("null")  # default value
+        optionmenu_select_online_dictionary = tk.OptionMenu(self, self.__selected_dictionary_type,
+                                                            *Translator.dictionary_type_tuple)
+        optionmenu_select_online_dictionary.grid(row=0, column=1)
+
+    def __command_button_word_confirm(self):
         input_word = self.entry_word.get()
         # 当没有输入时，点击无效
         if len(input_word) == 0:
+            # print(self.dictionary_type.get())
             return
         self.entry_word.delete(0, len(input_word))
         # 检测输入的是混淆单词列表还是单个单词
         input_word_list = input_word.split("-")
         if len(input_word_list) == 1:
-            self.__output_single_word_result_to_text_big(self, input_word)
-        else:
+            self.__output_single_word_result_to_text_show_all(input_word)
+        # else:
             # output_confused_word_result_to_display_widget(input_word_list)
 
+    # 点击导出按钮之后
+    def __command_button_output_confirm(self):
+        input_str = self.text_show_all.get("0.0", "end")
+        input_str = input_str.replace("\n", "\t")
+        input_str = input_str.replace("\t-----------------\t", "\r\n")
+        fo = codecs.open("C:\\Users\\78286\\Desktop\\foo.txt", "a+", "utf-8")
+        fo.write(input_str[0:-1])  # 去掉最后一个\t
+        fo.close()
+        self.text_show_all.delete("0.0", "end")  # 清空显示区
+        # display_message_widget.config(text="已导出")
+
     def __output_single_word_result_to_text_show_all(self, word):
-        translator = Translator(word, "youdao")
+        translator = Translator(word, self.__selected_dictionary_type.get())
         word_dictionary = translator.get_result_dictionary()
         input_string = word_dictionary["word"] + "\n" + \
                        word_dictionary["translation"] + "<br/>" + word_dictionary["example_chinese"] + "\n" + \
@@ -196,6 +229,9 @@ class Framework(tk.Tk):
                        word_dictionary["root"] + "\n"
         self.text_show_all.insert("end", input_string)
         self.text_show_all.insert("end", "-----------------\n")
+
+        self.label_vocabulary.config(text=word_dictionary["vocabulary_range"])
+
 
 window = Framework()
 window.mainloop()
